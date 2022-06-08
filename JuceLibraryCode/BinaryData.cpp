fac0f351c5705cbce51992c1de465bfeb1add8fc
Lucijan Busch <lucijan@directededge.com>
2022-06-08 09:03:04 +0200

    Update generated files

--- JuceLibraryCode/BinaryData.cpp
+++ JuceLibraryCode/BinaryData.cpp
@@ -41,6 +41,7 @@ static const unsigned char temp_binary_data_1[] =
 const char* stereo_svg = (const char*) temp_binary_data_1;
 
 
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes);
 const char* getNamedResource (const char* resourceNameUTF8, int& numBytes)
 {
     unsigned int hash = 0;
@@ -72,6 +73,7 @@ const char* originalFilenames[] =
     "stereo.svg"
 };
 
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
 const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8)
 {
     for (unsigned int i = 0; i < (sizeof (namedResourceList) / sizeof (namedResourceList[0])); ++i)
